d8e707df3af8d39c999574367f1b7a430553970c
Jenfacen <jzr5ve@virginia.edu>
2022-09-03 17:23:44 -0400

    initial commit

---
+++ Assets/StateMachines/Player/PlayerBaseState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerBaseState : State
+{
+    protected PlayerStateMachine StateMachine;
+
+    public PlayerBaseState(PlayerStateMachine StateMachine)
+    {
+        this.StateMachine = StateMachine;
+
+    }
+}
---
+++ Assets/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+
+    private float timer = 5f;
+
+    public PlayerTestState(PlayerStateMachine StateMachine) : base(StateMachine) { }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        timer =- deltaTime;
+        Debug.Log(timer);
+        if (timer <= 0)
+        {
+            StateMachine.SwitchState(new PlayerTestState(StateMachine));
+        }
+
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+       
+    }
+
+    
+}
